10d80b9b1ebe8e5f5faf790ae8bc4c80ec2bde1a
Andrey Kurilov <andrey.kurilov@emc.com>
2023-06-17 22:05:30 +0300

    save

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ function handleCredentialResponse(response) {
     const credential = response.credential;
     alert(credential)
 }
+
+function renderSignInButton() {
+    const auth2 = gapi.auth2.getAuthInstance();
+    auth2.attachClickHandler('signin-button', {}, handleCredentialResponse);
+}
+
+function loadGoogleAuth() {
+    gapi.load('auth2', function () {
+        gapi.auth2.init({
+            client_id: 'YOUR_CLIENT_ID',
+        }).then(renderSignInButton);
+    });
+}
+
+document.getElementById('signin-button').addEventListener('click', loadGoogleAuth);
