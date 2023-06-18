3cc7713fcc0b2a22ef1354f7e7a0ded742dbd5f8
Andrey Kurilov <andrey.kurilov@emc.com>
2023-06-18 06:22:46 +0300

    save

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 function handleAuthGoogle(response) {
     const credential = response.credential;
-    const idToken = credential.id_token;
     // Decode the JWT token
-    const decodedToken = jwt_decode(idToken);
+    const decodedToken = jwt_decode(credential);
     // Extract the user email from the decoded token
     const userEmail = decodedToken.email;
     // Use the userEmail as needed (e.g., send it in an email)
