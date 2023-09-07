f975b023dd18268970b5f5816ba2d0ec7bbdad97
andrei <18027129+akurilov@users.noreply.github.com>
2023-09-07 21:35:59 +0300

    rework the homepage

--- script.js
+++
@@ -1,8 +0,0 @@
-function handleAuthGoogle(response) {
-    const credential = response.credential;
-    // Decode the JWT token
-    const decodedToken = jwt_decode(credential);
-    // Extract the user email from the decoded token
-    const userEmail = decodedToken.email;
-    // TODO request to issue a client certificate by the email
-}
